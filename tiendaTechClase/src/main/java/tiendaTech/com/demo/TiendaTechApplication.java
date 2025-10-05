9d931489500ac063095bbc9b882419660c330ae7
dquesada90259 <145231337+dquesada90259@users.noreply.github.com>
2025-10-04 20:06:11 -0600

    Semana 2-3
    Internacionalizacion
    Thymeleaf
    Bootstrap
    Dependencias
    Fragmentos

---
+++ tiendaTechClase/src/main/java/tiendaTech/com/demo/TiendaTechApplication.java
@@ -0,0 +1,21 @@
+package tiendaTech.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class TiendaTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(TiendaTechApplication.class);
+
+        // Cambiar el puerto si es necesario 
+        Map<String, Object> props = new HashMap<>();
+        props.put("server.port", 9090);
+        app.setDefaultProperties(props);
+
+        app.run(args);
+    }
+}
