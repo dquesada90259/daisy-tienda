f54f4e720055a7f4e1a42a513c096bda05433d1f
dquesada90259 <145231337+dquesada90259@users.noreply.github.com>
2025-10-07 16:47:50 -0600

    Semana 4
    Categorias
    Controladores
    Fragmentos
    Listados

---
+++ tiendaTechClase/src/main/java/tiendaTech/services/CategoriaServices.java
@@ -0,0 +1,24 @@
+
+package tiendaTech.services;
+
+import tiendaTech.domain.Categoria;
+import tiendaTech.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+@Service
+public class CategoriaServices {
+ 
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+        }
+        return categoriaRepository.findAll();
+    }
+
+}
